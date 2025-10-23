b97a99a799d200705e65c0f62e41ae8f98918f23
4414 <4414shuma1004@gmail.com>
2025-10-23 14:56:09 +0900

    変更されたかな？

--- Assets/Scripts/WASD.cs
+++ Assets/Scripts/WASD.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class WASD : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    // GitHubÇ…îΩâfÇ≥ÇÍÇΩÇ©Ç»ÅH
+    // GitHub ni hyouji sareta ppoi!!
     void Start()
     {
         
