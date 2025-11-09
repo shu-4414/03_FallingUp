be0eb4e9a5e6394ec515ffe627255b8ffe20e895
4414 <4414shuma1004@gmail.com>
2025-11-09 20:56:19 +0900

    UI向けのタップ判定を管理するスクリプト。OnTap()の部分で、継承先で使い方を決めるようにする。

---
+++ Assets/Scripts/TapCollider.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapCollider : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (EventSystem.current == null)
+        {
+            Debug.LogWarning("EventSystemÇ™ÉVÅ[ÉìÇ…Ç†ÇËÇ‹ÇπÇÒÅB");
+        }
+        var CurrentTrigger = gameObject.AddComponent<EventTrigger>();
+
+        var EntryClick = new EventTrigger.Entry();
+        EntryClick.eventID = EventTriggerType.PointerClick; 
+        EntryClick.callback.AddListener((x) => OnTap());
+
+        CurrentTrigger.triggers.Add(EntryClick);
+    }
+
+    
+
+    protected virtual void OnTap()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        OnTap();
+    }
+}
